a8989f83aec69071cec1b44e4a2dca205ab6e034
Bruno Alfred <alfredybruno@gmail.com>
2020-05-11 13:32:41 +0300

    feat: Created the MainBody component of the App

---
+++ src/components/Maincontent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Maincontent = () => {
+    return (
+        <div>
+
+            This is the Place for my Content
+
+        </div>
+    );
+
+}
+
+export default Maincontent;
