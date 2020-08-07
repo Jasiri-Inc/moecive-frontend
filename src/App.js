0fd3c1f485de145e391efc0da1d6d595875ac18f
bruno alfred <alfredybruno@gmail.com>
2020-08-07 15:47:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ const App = () => {
   return (
     <div>
       <LoginPage />
-      <p>
-        <button onClick={methodDoesNotExist}>Break the world</button>
-      </p>
+   
     </div>
   );
 };
