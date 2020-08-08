f19915b1b4d141887da4ca3be8db72b1ce8385f8
bruno alfred <alfredybruno@gmail.com>
2020-08-08 05:16:37 +0300

    npm check for updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import LoginPage from "./components/Form";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <div>
-      <LoginPage />
-      
+      {/* <LoginPage /> */}
+      <Home/>
     </div>
   );
 };
