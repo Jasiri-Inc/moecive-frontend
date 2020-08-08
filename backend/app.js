7bbe46d1bb6849e26ccf4c45dccf650ac2e8fb1a
bruno alfred <alfredybruno@gmail.com>
2020-08-08 07:26:49 +0300

    exposed the rputes to app.js file

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
+const aboutRouter = require('./routes/about');
+const bookRouter = require('./routes/book');
+const booksRouter = require('./routes/books')
+
 
 const app = express();
 
@@ -20,7 +25,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', registerRouter);
+app.use('/about', aboutRouter);
+app.use('/login', loginRouter);
+app.use('/book', bookRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
