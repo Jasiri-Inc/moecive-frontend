95faf1db71414d2f98263c263a2b27e5310bc3c4
bruno alfred <alfredybruno@gmail.com>
2020-08-27 16:48:26 +0300

    Update login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
+const dotenv = require("dotenv");
+
+// get config vars
+dotenv.config();
+const secretToken = process.env.TOKEN_SECRET;
 
 router.post("/", (req, res, next) => {
   const user = {
@@ -8,7 +13,7 @@ router.post("/", (req, res, next) => {
     password: 1234,
   };
 
-  jwt.sign({ user }, "bruno", (err, token) => {
+  jwt.sign({ user }, secretToken, { expiresIn: "100000s" }, (err, token) => {
     res.json({
       token,
     });
