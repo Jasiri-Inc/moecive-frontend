2d35c984e389cc7b6a9e2b8b9818248a7c4a56df
bruno alfred <alfredybruno@gmail.com>
2020-08-27 16:48:31 +0300

    Update register.js

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Welcome to register Pages');
+  
 });
 
 module.exports = router;
