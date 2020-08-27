4a432bcec8b0b9198792120e7c07835b4e453e91
bruno alfred <alfredybruno@gmail.com>
2020-08-27 15:06:53 +0300

    implementing auth0

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,8 +1,18 @@
-const express  = require('express')
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('Welcome to Login Pages');
+router.post("/", (req, res, next) => {
+  const user = {
+    user: "Bruno",
+    password: 1234,
+  };
+
+  jwt.sign({ user }, "bruno", (err, token) => {
+    res.json({
+      token,
+    });
+  });
 });
 
 module.exports = router;
