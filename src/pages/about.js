83b077209651f08f966ab8c42d12cfab155a9492
Francis Gervase <francisgervase56@gmail.com>
2020-08-28 11:37:12 +0300

    moecive-v1 update2

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,13 @@ export default function About() {
     <div className="container">
       <div className="row mt-5 p-4">
         <h1>About page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, accusantium et iure voluptatibus eum illum ex vero deserunt? Veniam similique atque perferendis quo distinctio autem exercitationem vero nulla ipsum dolorem vitae tempora sapiente adipisci, odio esse facilis quis. Expedita, in.</p>
+        <p>Dodoma City is located in the centre of the country (6°10’23’’S; 35°44’31’’E), 455 km west of the former capital, Dar es Salaam; and 441 km south of Arusha City, the cradle of the East African Community. It is also 259 km north of Iringa Municipality through Mtera. The City covers an area of 2,669 km2 of which 625 km2 is urbanised.
+
+Dodoma features a semi-arid climate with relatively warm temperatures throughout the year. Although average maximums are consistent throughout the year, average minimums drop to 13°C in July. The average annual precipitation is 570 mm, most of which occurs during the wet season between November and April, with the remainder of the year comprising dry season.
+
+The recently refurbished Dodoma Airport and the Central Railway Line connecting it over a distance of 465 km with Dar es Salaam serve the city. There are also major highways connecting Dodoma with Dar es Salaam (via Morogoro Region) to the east, Mwanza City (via Singida) to the west, and Arusha (via Kondoa) to the north.
+
+The University is located about 8 km east of the city centre, and is accessible by public transport, which is easily available from the city centre. Given the central location of Dodoma, UDOM is strategically positioned to serve applicants around the country and specifically Government and private sector employees living in Dodoma, who hitherto could not find training opportunities in the area. Such employees can comfortably utilise UDOM to combine work and study for their career advancement. Additionally, the geographical location and Dodoma weather render UDOM a convenient place for international students.</p>
 
       </div>
     </div>
