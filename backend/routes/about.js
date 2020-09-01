128d50df7b40405eab3e946fbeb589d2381e1e64
bruno alfred <alfredybruno@gmail.com>
2020-09-01 06:09:55 +0300

    removed the backend from the front

--- backend/routes/about.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Get About Pages
-router.get('/', (req, res, next) => {
-    res.send('Welcome to about PAge')
-});
-
-module.exports = router; 
--- backend/routes/book.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router();
-
-// Get book Page
-router.get('/', (req, res, next) => {
-    res.send('Welcome to Book Page')
-})
-
-module.exports = router;
