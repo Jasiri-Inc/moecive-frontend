986fbd58714b4c3319fbdd2355cf8fe5e62fd515
bruno alfred <alfredybruno@gmail.com>
2020-09-07 02:52:52 +0300

    wip(feat): Registering Form Handling Data
    Ref

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -61,7 +61,7 @@ export default function Home() {
                   href="http://moecive.netlify.app/"
                   role="button"
                 >
-                  ✍🏽{" "}
+                 ✍🏽
                 </a>
               </p>
             </div>
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,60 +1,87 @@
-import React from "react";
-import { Col, Row, Form, Button, FormGroup, Label, Input } from "reactstrap";
+import React, { useState } from "react";
+import axios from "axios";
+import {useMutation} from "react-query";
+
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Form,
+  Button,
+  FormGroup,
+  Label,
+  Input,
+  Row,
+  Col,
+} from "reactstrap";
 
 export default function Register() {
+  const [useremail, setUseremail] = useState("");
+  const [password, setPassword] = useState("");
+
+const [mutate, {status, data, error}] = useMutation(
+  ({useremail,password}) => axios.post('http://127.0.0.1:5000/register', {useremail, password})
+)
+  const createUser = async e => {
+    await mutate({useremail, password})
+  }
   return (
-    <div className="d-flex justify-content-center " style={{paddingTop: 70}}>
-      <Form>
-        <Row form>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="exampleName">First Name</Label>
-              <Input
-                type="name"
-                name="name"
-                id="exampleName"
-                placeholder="First Name"
-              />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="exampleName">Last Name</Label>
-              <Input
-                type="name"
-                name="password"
-                id="examplePassword"
-                placeholder="Last Name"
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="examplePassword"
-            placeholder="strong password"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="examplePassword"
-            placeholder="Repeate the Strong password"
-          />
-        </FormGroup>
-        <FormGroup check>
-          <Input type="checkbox" name="check" id="exampleCheck" />
-          <Label for="exampleCheck" check>
-            Check me out
-          </Label>
-        </FormGroup>
-        <Button>Register</Button>
-      </Form>
+    <div className="d-flex justify-content-center " style={{ paddingTop: 70 }}>
+      <Card>
+        <CardBody>
+          <CardTitle>Register</CardTitle>
+
+          <Form onSubmit={createUser}>
+            <Row form>
+              <Col md={10}>
+                <FormGroup >
+                  <Label for="examplePassword">Email</Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    id="exampleEmail"
+                    placeholder="Your email"
+                    onChange={(event) => setUseremail(event.target.value)}
+                  />
+                    {console.log(useremail)}
+                  
+                </FormGroup>
+
+                <FormGroup>
+                  <Label for="examplePassword">Password</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    id="examplePassword"
+                    placeholder="strong password"
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                  {console.log(password)}
+                </FormGroup>
+                <FormGroup>
+                  <Label for="verifyPassword">Password</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    id="verifyPassword"
+                    placeholder="Repeate the Strong password"
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                  {console.log(password)}
+
+                </FormGroup>
+                <FormGroup check>
+                  <Input type="checkbox" name="check" id="exampleCheck" />
+                  <Label for="exampleCheck" check>
+                    Login after registering
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Button>Register</Button>
+          </Form>
+        </CardBody>
+      </Card>
     </div>
   );
 }
