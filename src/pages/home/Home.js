a9f5e351496844bb769b279bfab2ed4632aaf621
bruno alfred <alfredybruno@gmail.com>
2020-09-07 00:15:15 +0300

    wip(homePage): Added description
    Ref #17

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,75 +1,75 @@
 import React from "react";
-import  Footer  from "components/Footer";
+import Footer from "components/Footer";
 
 export default function Home() {
   return (
-      <div>
-    <main role="main">
-      {/* Main jumbotron for a primary marketing message or call to action */}
-      <div className="jumbotron">
+    <div>
+      <main role="main">
+      
+        <div className="jumbotron">
+          
+          <div className="container ">
+            <h1 className="display-3"> CIVE</h1>
+            <p>
+              <b>Welcome to College of Informatics and Virtual Education</b>
+            </p>
+            <p>
+              <a
+                className="btn btn-primary btn-lg"
+                href="http://moecive.netlify.app/"
+                role="button"
+              >
+                👽
+              </a>
+            </p>
+          </div>
+        </div>
         <div className="container">
-          <h1 className="display-3">Hello, world!</h1>
-          <p>
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-          </p>
-          <p>
-            <a className="btn btn-primary btn-lg" href="http://moecive.netlify.app/" role="button">
-              Learn more »
-            </a>
-          </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <h2>Heading</h2>
-            <p>
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            </p>
-            <p>
-              <a className="btn btn-secondary" href="http://moecive.netlify.app/" role="button">
-                View details »
-              </a>
-            </p>
+          <div className="row">
+            <div className="col-md-4">
+              <h2>Electronics and Telecommunication Enginnering</h2>
+              <p>asdasdsad</p>
+              <p>
+                <a
+                  className="btn btn-secondary"
+                  href="http://moecive.netlify.app/"
+                  role="button"
+                >
+                  ✍🏽
+                </a>
+              </p>
+            </div>
+            <div className="col-md-4">
+              <h2>Computer Science and Engineering</h2>
+              <p>sdasdas</p>
+              <p>
+                <a
+                  className="btn btn-secondary"
+                  href="http://moecive.netlify.app/"
+                  role="button"
+                >
+                  ✍🏽
+                </a>
+              </p>
+            </div>
+            <div className="col-md-4">
+              <h2>Information System and Technology</h2>
+              <p>sadsadsad</p>
+              <p>
+                <a
+                  className="btn btn-secondary"
+                  href="http://moecive.netlify.app/"
+                  role="button"
+                >
+                  ✍🏽{" "}
+                </a>
+              </p>
+            </div>
           </div>
-          <div className="col-md-4">
-            <h2>Heading</h2>
-            <p>
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-              
-            </p>
-            <p>
-              <a className="btn btn-secondary" href="http://moecive.netlify.app/" role="button">
-                View details »
-              </a>
-            </p>
-          </div>
-          <div className="col-md-4">
-            <h2>Heading</h2>
-            <p>
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            Welcome to our site || Welcome to our site || Welcome to our site
-            </p>
-            <p>
-              <a className="btn btn-secondary" href="http://moecive.netlify.app/" role="button">
-                View details »
-              </a>
-            </p>
-          </div>
-        </div>
-        <hr />
-      </div>{" "}
-    </main>
-    <Footer name="Bruno" />
+          <hr />
+        </div>{" "}
+      </main>
+      <Footer name="Bruno" />
     </div>
   );
 }
